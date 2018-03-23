b124a32a1771fbb62fdd148afc28b0cd1717e0cf
Derek Witteck <drwitteck@gmail.com>
2018-03-23 13:21:20 -0400

    Updated Karma testing dependencies for Chrome and Safari.

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -11,13 +11,29 @@ module.exports = function karmaConfig (config) {
     // 1. install corresponding karma launcher
     //    http://karma-runner.github.io/0.13/config/browsers.html
     // 2. add it to the `browsers` array below.
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome', 'Safari'],
     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
     reporters: ['spec', 'coverage'],
     files: ['./index.js'],
     preprocessors: {
       './index.js': ['webpack', 'sourcemap']
     },
+    plugins: [
+      // Launchers
+      'karma-chrome-launcher',
+
+      // Test Libraries
+      'karma-mocha',
+      'karma-sinon-chai',
+
+      // Preprocessors
+      'karma-webpack',
+      'karma-sourcemap-loader',
+
+      // Reporters
+      'karma-spec-reporter',
+      'karma-coverage'
+    ],
     webpack: webpackConfig,
     webpackMiddleware: {
       noInfo: true
