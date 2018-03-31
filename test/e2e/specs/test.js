2930a8980f7d03f8b558056b18f388de037b36a2
silvia <silviacu273@yahoo.com>
2018-03-31 01:17:19 -0400

    tests stuffs

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -10,9 +10,9 @@ module.exports = {
 
     browser
       .url(devServer)
-      .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
+      .waitForElementVisible('#logo', 5000)
+      .assert.elementPresent('.title')
+      .assert.containsText('span', 'Pic-a-Pup')
       .assert.elementCount('img', 1)
       .end()
   }
