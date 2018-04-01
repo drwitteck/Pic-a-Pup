5e455ca3a1f1d0b16c2e4ab4cf14e73d96f58585
Xiaoshanna <tuf48938@temple.edu>
2018-03-31 23:03:13 -0400

    updates to dashboard.spec.js

--- test/unit/specs/dashboard.spec.js
+++ test/unit/specs/dashboard.spec.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import DashboardPage from '../../../src/components/dashboard/dashboard.vue'
 
-describe('dashboard.vue', () => {
+describe('DashboardPage', () => {
+    it('sets the correct default data', () => {
+        expect(typeof DashboardPage.data).toBe('function')
+        const defaultData = DashboardPage.data()
+        expect(defaultData.name).toBe('defaultName')
+        expect(defaultData.picture).toBe('../pictures/pap.png')
+        expect(defaultData.imgData).toBe('')
+        expect(defaultData.profileImageData).toBe('')
+        expect(defaultData.uploadTask).toBe('')
+        expect(defaultData.downloadUrl).toBe('')
+        expect(defaultData.drawer).toBe(true)
+    })
     it(`navigation drawer's visibility should be controlled by a hamburger menu`, done => {
         const Constructor = Vue.extend(DashboardPage)
 
@@ -10,7 +21,23 @@ describe('dashboard.vue', () => {
         }).$mount()
         done()
     })
-    it(`navigation drawer's visibility should be controlled by a hamburger menu`, done => {
+    it(`Home link under navigation drawer should go to /dashboard`, done => {
+        const Constructor = Vue.extend(DashboardPage)
+
+        const comp = new Constructor({
+
+        }).$mount()
+        done()
+    })
+    it(`Pic-a-Breed link under navigation drawer should go to /breed`, done => {
+        const Constructor = Vue.extend(DashboardPage)
+
+        const comp = new Constructor({
+
+        }).$mount()
+        done()
+    })
+    it(`Sign Out button should sign you out and return you to login`, done => {
         const Constructor = Vue.extend(DashboardPage)
 
         const comp = new Constructor({
