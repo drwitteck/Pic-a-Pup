b8095758fb9e838c73805f59dea09f37b4476686
silvia <silviacu273@yahoo.com>
2018-03-31 21:06:09 -0400

    test stuff

--- test/unit/specs/signin.spec.js
+++ test/unit/specs/signin.spec.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import store from '../../../src/store.js'
 // The path is relative to the project root.
 import SigninPage from '../../../src/components/auth/signin.vue'
 
@@ -7,17 +8,21 @@ describe('SigninPage.vue', () => {
     // Extend the component to get the constructor, which we can then initialize directly.
     const Constructor = Vue.extend(SigninPage)
 
-    const comp = new Constructor({
-
+    const comp = new Constructor({ store
     }).$mount()
+    comp.password = '123123'
+    comp.email = 'pls@work.com'
+    comp.onSubmit()
+    console.log(localStorage)
+    // try to compare what is in the local store in the store.js to the user id in firebase
     done()
   })
-
   it(`sign in button works`, done => {
     const Constructor = Vue.extend(SigninPage)
 
     const comp = new Constructor({
     }).$mount()
+
     done()
   })
   it(`logs in succesfully, is redirected to dashboard`, done => {
