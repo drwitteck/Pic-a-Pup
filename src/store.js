ba117ec58c594d9b376a85ac8574f982e544b1e2
silvia <silviacu273@yahoo.com>
2018-04-01 21:37:29 -0400

    store vuex progression for tests

--- src/store.js
+++ src/store.js
@@ -8,110 +8,95 @@ import router from './router'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-    idToken: null,
-    userId: null,
-    user: null
-  },
-  mutations: {
-    authUser (state, userData) {
-      state.idToken = userData.token
-      state.userId = userData.userId
-    },
-    /**
-    storeUser (state, user) {
-      state.user = user
-    },
-     **/
-    clearAuthData (state) {
-      state.idToken = null
-      state.userId = null
-    }
-  },
-  actions: {
-    setLogoutTimer ({commit}, expirationTime) {
-      setTimeout(() => {
-        commit('clearAuthData')
-      }, expirationTime * 1000)
-    },
-    signup ({commit, dispatch}, authData) {
-      axios.post('/signupNewUser?key=AIzaSyDFULdMLBvglLWLD6m2sjyJ43g_rfjcBj4', {
-        email: authData.email,
-        password: authData.password,
-        returnSecureToken: true
-      })
-        .then(res => {
-          console.log(res)
-          commit('authUser', {
-            token: res.data.idToken,
-            userId: res.data.localId
-          })
-          authData.uuid = res.data.localId
-          const now = new Date()
-          const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000)
-          localStorage.setItem('token', res.data.idToken)
-          localStorage.setItem('userId', res.data.localId)
-          localStorage.setItem('expirationDate', expirationDate)
-          //dispatch('storeUser', authData)
-          dispatch('setLogoutTimer', res.data.expiresIn)
-          router.push('/dashboard')
+export const state = {
+  idToken: null,
+  userId: null,
+  user: null
+}
 
-        })
-        .catch(error => console.log(error))
-    },
-    login ({commit, dispatch}, authData) {
-      console.log(authData)
-      axios.post('/verifyPassword?key=AIzaSyDFULdMLBvglLWLD6m2sjyJ43g_rfjcBj4', {
-        email: authData.email,
-        password: authData.password,
-        returnSecureToken: true
-      })
-        .then(res => {
-          console.log(res)
-          const now = new Date()
-          const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000)
-          localStorage.setItem('token', res.data.idToken)
-          localStorage.setItem('userId', res.data.localId)
-          localStorage.setItem('expirationDate', expirationDate)
-          commit('authUser', {
-            token: res.data.idToken,
-            userId: res.data.localId
-          })
-          dispatch('setLogoutTimer', res.data.expiresIn)
-          router.push('/dashboard')
-
-        })
-        .catch(error => {//Please style below xiao :D
-          console.log(error)
-          console.log('login unsuccesful womp womp')
-          alert('Password or username was incorrect, please try again')
-        })
-    },
-    tryAutoLogin ({commit}) {
-      const token = localStorage.getItem('token')
-      if (!token) {
-        return
-      }
-      const expirationDate = localStorage.getItem('expirationDate')
-      const now = new Date()
-      if (now >= expirationDate) {
-        return
-      }
-      const userId = localStorage.getItem('userId')
-      commit('authUser', {
-        token: token,
-        userId: userId
-      })
-    },
-    logout ({commit}) {
+export const actions = {
+  setLogoutTimer: ({commit}, expirationTime) => {
+    setTimeout(() => {
       commit('clearAuthData')
-      localStorage.removeItem('expirationDate')
-      localStorage.removeItem('token')
-      localStorage.removeItem('userId')
-      router.replace('/signin')
-    },
-    /** storeUser ({commit, state}, userData) {
+    }, expirationTime * 1000)
+  },
+  signup: ({commit, dispatch}, authData) => {
+    axios.post('/signupNewUser?key=AIzaSyDFULdMLBvglLWLD6m2sjyJ43g_rfjcBj4', {
+      email: authData.email,
+      password: authData.password,
+      returnSecureToken: true
+    })
+      .then(res => {
+        console.log(res)
+        commit('authUser', {
+          token: res.data.idToken,
+          userId: res.data.localId
+        })
+        authData.uuid = res.data.localId
+        const now = new Date()
+        const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000)
+        localStorage.setItem('token', res.data.idToken)
+        localStorage.setItem('userId', res.data.localId)
+        localStorage.setItem('expirationDate', expirationDate)
+        //dispatch('storeUser', authData)
+        dispatch('setLogoutTimer', res.data.expiresIn)
+        router.push('/dashboard')
+
+      })
+      .catch(error => console.log(error))
+  },
+  login: ({commit, dispatch}, authData) => {
+    console.log(authData)
+    axios.post('/verifyPassword?key=AIzaSyDFULdMLBvglLWLD6m2sjyJ43g_rfjcBj4', {
+      email: authData.email,
+      password: authData.password,
+      returnSecureToken: true
+    })
+      .then(res => {
+        console.log(res)
+        const now = new Date()
+        const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000)
+        localStorage.setItem('token', res.data.idToken)
+        localStorage.setItem('userId', res.data.localId)
+        localStorage.setItem('expirationDate', expirationDate)
+        commit('authUser', {
+          token: res.data.idToken,
+          userId: res.data.localId
+        })
+        dispatch('setLogoutTimer', res.data.expiresIn)
+        router.push('/dashboard')
+
+      })
+      .catch(error => {//Please style below xiao :D
+        console.log(error)
+        console.log('login unsuccesful womp womp')
+        alert('Password or username was incorrect, please try again')
+      })
+  },
+  tryAutoLogin ({commit}) {
+    const token = localStorage.getItem('token')
+    if (!token) {
+      return
+    }
+    const expirationDate = localStorage.getItem('expirationDate')
+    const now = new Date()
+    if (now >= expirationDate) {
+      return
+    }
+    const userId = localStorage.getItem('userId')
+    commit('authUser', {
+      token: token,
+      userId: userId
+    })
+  },
+  logout: ({commit}) => {
+    commit('clearAuthData')
+    localStorage.removeItem('expirationDate')
+    localStorage.removeItem('token')
+    localStorage.removeItem('userId')
+    router.replace('/signin')
+  },
+  /** storeUser ({commit, state}, userData) {
       if (!state.idToken) {
         return
       }
@@ -119,7 +104,7 @@ export default new Vuex.Store({
         .then(res => console.log(res))
         .catch(error => console.log(error))
     },
-    fetchUser ({commit, state}) {
+   fetchUser ({commit, state}) {
       if (!state.idToken) {
         return
       }
@@ -129,13 +114,34 @@ export default new Vuex.Store({
         })
         .catch(error => console.log(error))
     }**/
-  },
-  getters: {
-    user (state) {
-      return state.user
-    },
-    isAuthenticated (state) {
-      return state.idToken !== null
-    }
+  setUser: (newUser) => {
+    state.user = newUser
   }
+}
+
+export const getters = {
+  user: (state) => {
+    return state.user
+  },
+  isAuthenticated: (state) => {
+    return state.idToken !== null
+  }
+}
+
+export const mutations ={
+  authUser: (state, userData) => {
+    state.idToken = userData.token
+    state.userId = userData.userId
+  },
+  clearAuthData: (state) => {
+    state.idToken = null
+    state.userId = null
+  }
+}
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  getters
 })
