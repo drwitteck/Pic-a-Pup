a3b1f60981dfff6997227c668ec16ac42be95f48
silvia <silviacu273@yahoo.com>
2018-04-20 14:13:19 -0400

    profile

---
+++ src/components/profile/profileinfo.vue
@@ -0,0 +1,40 @@
+/* eslint-disable */
+<template>
+  <div id="profileinfo">
+    <h1>Profile Info</h1>
+    <v-layout row wrap>
+      <v-flex xs4 offset-sm1>
+      </v-flex>
+      <v-flex xs4>
+      </v-flex>
+    </v-layout>
+    <div>
+      <v-layout row wrap>
+        <br/>
+        <v-flex xs6 offset-sm1>
+        </v-flex>
+        <v-flex xs4>
+        </v-flex>
+      </v-layout>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      profilepicture: '',
+      name: '',
+      zip: '',
+      proserv: false,
+      recserv: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
